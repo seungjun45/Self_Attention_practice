9d42d4ae4f38ac7db6800fb1b158c345333870cf
Seungjun <seungjun45@kaist.ac.kr>
2019-05-02 14:32:13 +0900

    Squeeze and Excitation Res34 & 50

---
+++ README.md
@@ -0,0 +1,2 @@
+# Self_Attention_practice
+Pytorch implementation of Squeeze_and_Excitation, Bottleneck_Attention_Module, and Convolutional_Block_Attention_Module : for EE898 class, KAIST
---
+++ utils.py
@@ -0,0 +1,37 @@
+import torch
+
+def save_model(path, ResNet, SqueezeExcite, epoch, optimizer=None):
+    SqueezeExcite_states=[]
+    for SE_param in SqueezeExcite:
+        SqueezeExcite_states.append(SE_param[0].state_dict())
+        SqueezeExcite_states.append(SE_param[1].state_dict())
+    model_dict = {
+            'epoch': epoch,
+            'ResNet_state': ResNet.state_dict(),
+            'SqueezeExcite_state': SqueezeExcite_states
+        }
+    if optimizer is not None:
+        model_dict['optimizer_state'] = optimizer.state_dict()
+
+    torch.save(model_dict, path)
+
+def save_model(Adapter_path, Adapter, epoch, optimizer=None):
+    SqueezeExcite_states=[]
+    for SE_param in SqueezeExcite:
+        SqueezeExcite_states.append(SE_param[0].state_dict())
+        SqueezeExcite_states.append(SE_param[1].state_dict())
+
+    print('loading Adapter from {}'.format(Adapter_path))
+    Adapter_model=torch.load(Adapter_path)
+    Adapter.ResNet.load_state_dict(Adapter_model['ResNet_state'])
+
+
+    model_dict = {
+            'epoch': epoch,
+            'ResNet_state': ResNet.state_dict(),
+            'SqueezeExcite_state': SqueezeExcite_states
+        }
+    if optimizer is not None:
+        model_dict['optimizer_state'] = optimizer.state_dict()
+
+    torch.save(model_dict, path)
